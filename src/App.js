696e131566c364188ef43b90309655f68ce117c1
Aiker Ary Acosta Cantillo <aikiringo26@gmail.com>
2023-08-24 14:57:21 -0500

    Starting project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import logo from './logo.svg';
-import './App.css';
+import './css/App.css';
+// Import please the useEffect and useState hooks from React, also fragment
+import React, { useEffect, useState, Fragment } from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // Create a fragment
+    <Fragment>
+      <h1>React Hooks</h1>
+      <p>UseEffect and useState</p>
+    </Fragment>
   );
 }
 
